79b06090f70a5e5b6fac6d63072146b7e1b92f2a
Bushra Baig <141634179+Bushra-Baig@users.noreply.github.com>
2024-11-25 05:19:17 -0800

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import MuiBook from './component/MuiBook';
+
+function App() {
+  return (
+    <div className="App">
+     <MuiBook/>
+    </div>
+  );
+}
+
+export default App;
